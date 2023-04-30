aa49039c21902db1c44733b1ca146464b5af731f
OkamiXX <cosunai@hotmail.com>
2023-04-30 14:39:01 -0500

    Tought i had to do it by myself so i started again

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
-var password = "";
-function pop() 
-{
-    let password = prompt("Whats the pass?");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-console.log(password);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
